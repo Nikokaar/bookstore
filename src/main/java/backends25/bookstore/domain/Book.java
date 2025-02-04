1687628b2c5abe0505284113617aea2b83dc770c
Niko Kaarmila <bha523@myy.haaga-helia.fi>
2025-02-04 16:19:03 +0200

    First commit

---
+++ src/main/java/backends25/bookstore/domain/Book.java
@@ -0,0 +1,57 @@
+package backends25.bookstore.domain;
+
+public class Book {
+
+    String title, author, publicationYear, isbn, price;
+
+    public Book() {
+        
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getPublicationYear() {
+        return publicationYear;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setPublicationYear(String publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Book [title=" + title + ", author=" + author + ", publicationYear=" + publicationYear + ", isbn=" + isbn
+                + ", price=" + price + "]";
+    }
+
+}
