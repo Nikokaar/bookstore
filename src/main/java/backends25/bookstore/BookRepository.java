e1ad2481dc55fdc08e90a80a1c55f7818add0dd8
Niko Kaarmila <bha523@myy.haaga-helia.fi>
2025-03-05 19:24:53 +0200

    Dependency added to pov nad to application.properties

--- src/main/java/backends25/bookstore/BookRepository.java
+++ src/main/java/backends25/bookstore/BookRepository.java
@@ -1,9 +1,12 @@
 package backends25.bookstore;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import backends25.bookstore.domain.Book;
 
 public interface BookRepository extends CrudRepository<Book, Long>  {
+    List<Book> findByCategoryName(String name);
 
 }
--- src/main/java/backends25/bookstore/web/BookStoreRestController.java
+++ src/main/java/backends25/bookstore/web/BookStoreRestController.java
@@ -1,12 +1,15 @@
 package backends25.bookstore.web;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -50,6 +53,29 @@ public class BookStoreRestController {
     Optional<Book> getBook(@PathVariable Long id) {
         log.info("find book, id = " + id);
         return bookRepository.findById(id);
+    }
+
+      // delete book
+    @DeleteMapping("/books/{id}")
+    public Iterable<Book> deleteBook(@PathVariable Long id) {
+        log.info("delete book, id = " + id);
+        bookRepository.deleteById(id);
+        return bookRepository.findAll();
+    }
+
+     // edit existing book information
+    @PutMapping("/books/{id}")
+    Book editBook(@RequestBody Book editedBook, @PathVariable Long id) {
+        log.info("edit book " + editedBook);
+        editedBook.setId(id);
+        return bookRepository.save(editedBook);
+    }
+
+    @GetMapping("/books/category/{name}")
+    List<Book> getBookByCategory(@PathVariable String name) {
+        log.info("find books, category = " + name);
+        return bookRepository.findByCategoryName(name);
+    
     }
     
 }
